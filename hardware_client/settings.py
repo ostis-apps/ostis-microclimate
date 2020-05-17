9eb73342772848a35b2a9821b7c711286d58a3b3
Pavel Antsypovich <pavelantsypovich@gmail.com>
2020-05-17 21:14:37 +0300

    added licence

--- hardware_client/settings.py
+++ hardware_client/settings.py
@@ -7,4 +7,4 @@ DIRPATH =  os.path.dirname(os.path.realpath(__file__))
 CONFIGFILE = os.path.join(DIRPATH, 'configs.json')
 CONFIG_FIELDNAMES = ['port', 'baudrate', 'timeout']
 
-REFRESH_SECONDS_INTERVAL = 10
+REFRESH_SECONDS_INTERVAL = 3
